0b302d1539151b40d60a332656be70ef07140046
한성진 <trstyq@gmail.com>
2024-07-04 00:39:11 +0900

    PushTopic 받아서 다른 오그에 해당 데이터 생성 완료. 라우팅 한바퀴 돌림

--- src/main/java/com/apache/sfdc/common/SalesforceDTOGenerator.java
+++ src/main/java/com/apache/sfdc/common/SalesforceDTOGenerator.java
@@ -14,13 +14,15 @@ import java.io.PrintWriter;
 @SpringBootApplication
 public class SalesforceDTOGenerator {
 
-    private static final String INSTANCE_URL = "https://ecologysyncmanagement-dev-ed.develop.my.salesforce.com/";
+    private static final String INSTANCE_URL = "https://posco--scrum4.sandbox.my.salesforce.com/";
 
     public static void main(String[] args) throws Exception {
 
+        // 단순 요청 보내기엔 OkHttp가 제일 좋은것 같음.
+        // 아직 Non-Block I/O가 필요한건 아니기에 Spring WebClient는 잠시 봉인
         OkHttpClient client = new OkHttpClient();
 
-        String sObjectName = "Account";  // SObject 이름
+        String sObjectName = "AllFieldType__c"; // SObject 이름
         Request request = new Request.Builder()
                 .url(INSTANCE_URL + "/services/data/v60.0/sobjects/" + sObjectName + "/describe")
                 .addHeader("Authorization", "Bearer " + SalesforceOAuth.getAccessToken())
@@ -28,11 +30,13 @@ public class SalesforceDTOGenerator {
 
         Response response = client.newCall(request).execute();
 
-        System.out.println(response.body());
-
         if (response.isSuccessful()) {
             String responseBody = response.body().string();
+
+            // 잭슨으로 역직렬화
             ObjectMapper objectMapper = new ObjectMapper();
+
+            // 세일즈 포스로 따지면 JSON.deserializeUntyped();
             JsonNode rootNode = objectMapper.readTree(responseBody);
             generateDTOClass(rootNode, sObjectName);
         } else {
@@ -43,27 +47,36 @@ public class SalesforceDTOGenerator {
     private static void generateDTOClass(JsonNode rootNode, String sObjectName) throws FileNotFoundException {
         StringBuilder classBuilder = new StringBuilder();
         classBuilder.append("package com.apache.sfdc.router.dto;\n\n");
-        classBuilder.append("import lombok.Data;\n\n");
+        classBuilder.append("import lombok.Data;\n");
+        classBuilder.append("import java.sql.Time;\n");
+        classBuilder.append("import java.time.LocalDate;\n");
+        classBuilder.append("import java.time.LocalDateTime;\n\n");
         classBuilder.append("@Data \n");
         classBuilder.append("public class ").append(sObjectName).append(" {\n");
 
         JsonNode fields = rootNode.get("fields");
         for (JsonNode field : fields) {
-            String fieldName = field.get("name").asText();
             String fieldType = field.get("type").asText();
             String javaType = mapToJavaType(fieldType);
+            String fieldName = field.get("name").asText();
+            String fieldLabel = field.get("label").asText();
 
-            classBuilder.append("    private ").append(javaType).append(" ").append(fieldName).append(";\n");
+            classBuilder
+                    .append(" private ")
+                    .append(javaType)
+                    .append(" ")
+                    .append(fieldName)
+                    .append("; // ")
+                    .append(fieldLabel)
+                    .append("\n");
         }
 
         classBuilder.append("}\n");
 
         // 출력 디렉토리 생성
         File dir = new File("src/main/java/com/apache/sfdc/router/dto");
-        boolean result;
         if (!dir.exists()) {
-            result = dir.mkdirs();
-            System.out.println("기존 경로 존재 여부 : " + result);
+            dir.mkdirs();
         }
 
         // 파일 생성
@@ -75,14 +88,14 @@ public class SalesforceDTOGenerator {
     }
 
     private static String mapToJavaType(String fieldType) {
-        
-        // 타입 더 쪼개기
+
+        // 세일즈포스 타입을 자바 타입으로 쪼개기. 날짜 관련 필드는 나중에 무결성 확인 요
         return switch (fieldType) {
-            case "string" -> "String";
             case "boolean" -> "Boolean";
-            case "int" -> "Integer";
-            case "double" -> "Double";
-            // 추가적인 필드 타입 매핑
+            case "date" -> "LocalDate";
+            case "datetime" -> "LocalDateTime";
+            case "time" -> "Time";
+            case "double", "currency", "percent" -> "Double";
             default -> "String";
         };
     }
--- src/main/java/com/apache/sfdc/router/modal/service/RouterServiceImpl.java
+++ src/main/java/com/apache/sfdc/router/modal/service/RouterServiceImpl.java
@@ -1,5 +1,7 @@
 package com.apache.sfdc.router.modal.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Message;
@@ -10,51 +12,85 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class RouterServiceImpl implements RouterService{
 
-    @Value("${camel.component.salesforce.client-id}")
-    private String clientId;
+    @Value("${camel.component.salesforce-ecology.login-url}")
+    private String ecologyLoginUrl;
 
-    @Value("${camel.component.salesforce.client-secret}")
-    private String clientSecret;
+    @Value("${camel.component.salesforce-ecology.client-id}")
+    private String ecologyClientId;
 
-    @Value("${camel.component.salesforce.user-name}")
-    private String userName;
+    @Value("${camel.component.salesforce-ecology.client-secret}")
+    private String ecologyClientSecret;
 
-    @Value("${camel.component.salesforce.password}")
-    private String password;
+    @Value("${camel.component.salesforce-ecology.user-name}")
+    private String ecologyUserName;
 
-    @Value("${camel.component.salesforce.login-url}")
-    private String loginUrl;
+    @Value("${camel.component.salesforce-ecology.password}")
+    private String ecologyPassword;
+
+    @Value("${camel.component.salesforce-sj.login-url}")
+    private String SungJinDevLoginUrl;
+
+    @Value("${camel.component.salesforce-sj..client-id}")
+    private String SungJinDevClientId;
+
+    @Value("${camel.component.salesforce-sj.client-secret}")
+    private String SungJinDevClientSecret;
+
+    @Value("${camel.component.salesforce-sj.user-name}")
+    private String SungJinDevUserName;
+
+    @Value("${camel.component.salesforce-sj.password}")
+    private String SungJinDevPassword;
+
     public void SetSubCDC(String changeEvent) throws Exception {
 
-        SalesforceComponent sfComponent_1 = new SalesforceComponent();
-        sfComponent_1.setLoginUrl(loginUrl);
-        sfComponent_1.setClientId(clientId);
-        sfComponent_1.setClientSecret(clientSecret);
-        sfComponent_1.setUserName(userName);
-        sfComponent_1.setPassword(password);
-        sfComponent_1.setPackages("com.apache.sfdc.router.dto");
+        SalesforceComponent myDev = new SalesforceComponent();
+        myDev.setLoginUrl(ecologyLoginUrl);
+        myDev.setClientId(ecologyClientId);
+        myDev.setClientSecret(ecologyClientSecret);
+        myDev.setUserName(ecologyUserName);
+        myDev.setPassword(ecologyPassword);
+        myDev.setPackages("com.apache.sfdc.router.dto");
+
+        SalesforceComponent scrumDev = new SalesforceComponent();
+        scrumDev.setLoginUrl(SungJinDevLoginUrl);
+        scrumDev.setClientId(SungJinDevClientId);
+        scrumDev.setClientSecret(SungJinDevClientSecret);
+        scrumDev.setUserName(SungJinDevUserName);
+        scrumDev.setPassword(SungJinDevPassword);
+        scrumDev.setPackages("com.apache.sfdc.router.dto");
 
         RouteBuilder rb = new RouteBuilder() {
             @Override
             public void configure() throws Exception {
                 System.out.println("실행");
-                // Salesforce Pub/Sub API 구독 설정
-                from("sfComponent1:subscribe:data/" + changeEvent +"?rawPayload=true")
+
+                from("myDev:subscribe:" + changeEvent)
                         .process(exchange -> {
-                            Message in = exchange.getIn();
-                            System.out.println(in.getBody());
-                        });
+                            Message message = exchange.getIn();
+                            System.out.println(message.getBody());
+
+                            ObjectMapper objectMapper = new ObjectMapper();
+                            Map<String, Object> map = objectMapper.convertValue(message.getBody(), Map.class);
+                            map.remove("Id");
+
+                            exchange.getIn().setBody(new Gson().toJson(map));
+                        })
+                        .to("scrumDev:createSObject?sObjectName=" + changeEvent)
+                        .log("Created new Account with ID ${body.id}");
             }
         };
 
         CamelContext myCamelContext = new DefaultCamelContext();
         myCamelContext.addRoutes(rb);
-        myCamelContext.addComponent("sfComponent1", sfComponent_1);
+        myCamelContext.addComponent("myDev", myDev);
+        myCamelContext.addComponent("scrumDev", scrumDev);
 
         try{
             myCamelContext.start();
